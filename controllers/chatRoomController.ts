37f7be9bb6b4205993ff622bef38f39c6b704a3e
Cristobal <cristobal@kimche.cl>
2024-03-08 11:47:49 +0000

    feat: add post message in controller

--- controllers/chatRoomController.ts
+++ controllers/chatRoomController.ts
@@ -3,6 +3,7 @@ import {
   getChatRooms,
   getMessagesFromChatRoom,
 } from '../services/chatRoomService';
+import { createMessage } from '../services/messageService';
 
 export const getMessagesByRoomId = async (req, res) => {
   try {
@@ -25,6 +26,34 @@ export const getAllAvaliableRooms = async (req, res) => {
     const chatRooms = await getChatRooms();
 
     return res.status(200).json({ success: true, rooms: chatRooms });
+  } catch (error) {
+    return res.status(500).json({ success: false, error });
+  }
+};
+
+export const postMessage = async (req, res) => {
+  try {
+    const { chatRoomId } = req.params;
+    const { message } = req.body;
+
+    if (!message) {
+      return res.status(400).json({ success: false, message: 'Message is required' });
+    }
+
+    const chatRoom = await getChatRoomById(chatRoomId);
+
+    if (!chatRoom) {
+      return res.status(400).json({ success: false, message: 'Chat room not found' });
+    }
+    const userId = req.userId;
+
+    try {
+      const createdMessage = await createMessage(message, userId, chatRoomId);
+
+      return res.status(200).json({ success: true, createdMessage });
+    } catch (error) {
+      return res.status(500).json({ success: false, error });
+    }
   } catch (error) {
     return res.status(500).json({ success: false, error });
   }
