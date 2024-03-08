c8809a8eab21eb49a17f6f2bf6bafbe459e4f037
Cristobal <cristobal@kimche.cl>
2024-03-08 11:48:22 +0000

    feat: add route to chatroom

--- routes/chatRoom.ts
+++ routes/chatRoom.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 
-import { getMessagesByRoomId, getAllAvaliableRooms } from '../controllers/chatRoomController';
+import {
+  getMessagesByRoomId,
+  getAllAvaliableRooms,
+  postMessage,
+} from '../controllers/chatRoomController';
 const router = express.Router();
 
-router.get('/:chatRoomId', getMessagesByRoomId);
 router.get('/', getAllAvaliableRooms);
+router.get('/:chatRoomId', getMessagesByRoomId);
+router.post('/:chatRoomId/message', postMessage);
 
 export default router;
