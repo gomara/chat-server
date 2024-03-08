094557593e0f306316290fb298f1ec2c17f58eec
Cristobal <cristobal@kimche.cl>
2024-03-08 11:15:37 +0000

    feat: add encrypt and validate password

--- utils/password_utils.ts
+++ utils/password_utils.ts
@@ -1,3 +1,20 @@
-export const encryptPassword = async (password: string | null | undefined): Promise<string> => {
-  return password;
+const workFactor = 8;
+
+import bcrypt from 'bcrypt';
+
+export const encryptPassword = async (password: string): Promise<string> => {
+  const salt = await bcrypt.genSalt(workFactor);
+  const hash = await bcrypt.hash(password, salt);
+
+  return hash;
+};
+
+export const validatePassword = async (
+  incommingPassword: string,
+  dataBasePassword: string,
+): Promise<boolean> => {
+  if (incommingPassword.length === 0) return false;
+  const decode = await bcrypt.compare(incommingPassword, dataBasePassword);
+
+  return decode;
 };
