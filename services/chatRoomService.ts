32af60d5a9a72819369e7264916b7ff347836ad1
Cristobal <cristobal@kimche.cl>
2024-03-08 09:15:23 +0000

    feat: add chatroom service

---
+++ services/chatRoomService.ts
@@ -0,0 +1,20 @@
+import { ChatRoomModel } from '../models/ChatRoom';
+import { MessageModel } from '../models/Message';
+
+export const getMessagesFromChatRoom = async (chatRoomId: string) => {
+  const messages = await MessageModel.find({ chatRoomId }).sort({ createdAt: 1 });
+
+  return messages;
+};
+
+export const getChatRoomById = async (chatRoomId: string) => {
+  const chatRoom = await ChatRoomModel.findById(chatRoomId);
+
+  return chatRoom;
+};
+
+export const getChatRooms = async () => {
+  const chatRooms = await ChatRoomModel.find();
+
+  return chatRooms;
+};
