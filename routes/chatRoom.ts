badf8c759e9a21415521f07b66d55f3229eea2f2
Cristobal <cristobal@kimche.cl>
2024-03-09 16:17:38 +0100

    feat: add routes

--- routes/chatRoom.ts
+++ routes/chatRoom.ts
@@ -4,6 +4,8 @@ import {
   getMessagesByRoomId,
   getAllAvaliableRooms,
   postMessage,
+  deleteMessage,
+  joinToChatRoomPost,
 } from '../controllers/chatRoomController';
 import { checkIfUserIsInRoom } from '../middleware/chatRoomMiddleware';
 const router = express.Router();
@@ -11,5 +13,7 @@ const router = express.Router();
 router.get('/', getAllAvaliableRooms);
 router.get('/:chatRoomId', getMessagesByRoomId);
 router.post('/:chatRoomId/message', checkIfUserIsInRoom, postMessage);
+router.post('/:chatRoomId', joinToChatRoomPost);
+router.delete('/:chatRoomId/message/:messageId', checkIfUserIsInRoom, deleteMessage);
 
 export default router;
