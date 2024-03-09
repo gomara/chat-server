419c3c6ead016b42b2c44e53f430935159c2e834
Cristobal <cristobal@kimche.cl>
2024-03-09 16:21:46 +0100

    chore: add jest and supertest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  bail: false,
+  testTimeout: 50000,
+  clearMocks: true,
+  collectCoverage: true,
+  // coverageReporters: ['html'], // not collect de data
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['./**/__tests__/auth.test.ts'],
+  verbose: true,
+};
