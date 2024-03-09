07f9c73b9715987b0bbc45f88ed083038cf5e3e1
Cristobal <cristobal@kimche.cl>
2024-03-09 16:15:17 +0100

    feat: add delete and joint controller

--- controllers/chatRoomController.ts
+++ controllers/chatRoomController.ts
@@ -1,8 +1,10 @@
+import { MessageModel } from '../models/Message';
 import {
   getChatRoomById,
   getChatRooms,
   getMessagesFromChatRoom,
 } from '../services/chatRoomService';
+import { joinUserToChatRoom } from '../services/chatRoomService';
 import { createMessage } from '../services/messageService';
 
 export const getMessagesByRoomId = async (req, res) => {
@@ -51,6 +53,37 @@ export const postMessage = async (req, res) => {
       const createdMessage = await createMessage(message, userId, chatRoomId);
 
       return res.status(200).json({ success: true, createdMessage });
+    } catch (error) {
+      return res.status(500).json({ success: false, error });
+    }
+  } catch (error) {
+    return res.status(500).json({ success: false, error });
+  }
+};
+
+export const deleteMessage = async (req, res) => {
+  try {
+    const { messageId } = req.params;
+
+    await MessageModel.findByIdAndDelete(messageId);
+
+    return res.status(200).json({ success: true, message: 'Message deleted successfully' });
+  } catch (error) {
+    return res.status(500).json({ success: false, error });
+  }
+};
+
+export const joinToChatRoomPost = async (req, res) => {
+  try {
+    const { chatRoomId } = req.params;
+    const userId = req.userId;
+
+    try {
+      await joinUserToChatRoom(null, chatRoomId, null, userId);
+
+      return res
+        .status(200)
+        .json({ success: true, message: `Succesfully joined to the chat room ${chatRoomId}` });
     } catch (error) {
       return res.status(500).json({ success: false, error });
     }
