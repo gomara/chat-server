8c17f1b098d5c549d35ecba571c9abbbadb210d0
Cristobal <cristobal@kimche.cl>
2024-03-10 07:16:14 +0100

    feat: add user generator from seed

--- db/seeder.ts
+++ db/seeder.ts
@@ -2,13 +2,17 @@ import { ChatRoomModel } from '../models/ChatRoom';
 import { UserModel } from '../models/User';
 
 import { chatRoomGenerator } from './generators/chatRoom_generator';
-import { userGenerator } from './generators/user_generator';
+import { userGenerator, userGeneratorFromEnv } from './generators/user_generator';
 
-export const seedUsers = async (userNumber = 1): Promise<void> => {
+export const seedUsers = async (): Promise<void> => {
   await UserModel.deleteMany();
+  const userName = process.env.USERNAME as string;
+  const password = process.env.PASSWORD as string;
 
-  for (let i = 0; i < userNumber; i++) {
+  if (!userName || !password) {
     await userGenerator();
+  } else {
+    await userGeneratorFromEnv({ username: userName, password: password });
   }
 };
 
